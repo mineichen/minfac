8235e447370e9e919fcfe23f9c21926b46a3c3d0
Markus Ineichen <kusi.ineichen@gmail.com>
2021-01-27 14:32:47 +0100

    Move Family-Types into private mod

---
+++ src/family_lifetime.rs
@@ -0,0 +1,53 @@
+use {
+    core::{
+        any::Any,
+        marker::PhantomData
+    }
+};
+// The family trait for type constructors that have one input lifetime.
+pub trait FamilyLt<'a> {
+    type Out: 'a;
+}
+
+#[derive(Debug)]
+pub struct IdFamily<T: Any>(PhantomData<T>);
+impl<'a, T: 'a + Any> FamilyLt<'a> for IdFamily<T> {
+    type Out = T;
+}
+
+#[derive(Debug)]
+pub struct RefFamily<T: Any>(PhantomData<T>);
+impl<'a, T: 'a + Any> FamilyLt<'a> for RefFamily<T> {
+    type Out = &'a T;
+}
+
+impl<'a, T: FamilyLt<'a> + 'a> FamilyLt<'a> for Option<T> {
+    type Out = Option<T::Out>;
+}
+
+impl<'a, T0: FamilyLt<'a> + 'a, T1: FamilyLt<'a> + 'a> FamilyLt<'a> for (T0, T1) {
+    type Out = (
+        <T0 as FamilyLt<'a>>::Out,
+        <T1 as FamilyLt<'a>>::Out
+    );
+}
+impl<'a, T0: FamilyLt<'a> + 'a, T1: FamilyLt<'a> + 'a, T2: FamilyLt<'a> + 'a> FamilyLt<'a> for (T0, T1, T2) {
+    type Out = (
+        <T0 as FamilyLt<'a>>::Out,
+        <T1 as FamilyLt<'a>>::Out,
+        <T2 as FamilyLt<'a>>::Out
+    );
+}
+impl<'a, T0: FamilyLt<'a>, T1: FamilyLt<'a>, T2: FamilyLt<'a>, T3: FamilyLt<'a>> FamilyLt<'a> for (T0, T1, T2, T3) {
+    type Out = (
+        <T0 as FamilyLt<'a>>::Out,
+        <T1 as FamilyLt<'a>>::Out,
+        <T2 as FamilyLt<'a>>::Out,
+        <T3 as FamilyLt<'a>>::Out
+    );
+}
+pub struct ServiceIteratorFamily<T>(PhantomData<T>);
+
+impl<'a, T: crate::resolvable::Resolvable> FamilyLt<'a> for ServiceIteratorFamily<T> {
+    type Out = crate::ServiceIterator<'a, T>;
+}
